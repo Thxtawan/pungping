f549cfc8fa3d2528852b56659cb85486761ca3e4
Thxtawan <pung242561@gmail.com>
2025-06-23 15:14:57 +0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+province =["กรุงเทพมหานค","กาฬสินธุ์","กาญจนบุรี","ฉะเชิงเทรา","ชลบุรี","พะเยา","ชุมพร","ชัยนาท","ขอนแก่น","ชัยภูมิ","ยโสธร","นครพนม","นครราชสีมา","เชียงใหม่"]
+province[0] = "Bangkok"
+province[12] = "Korat"
+province[3] = "Paet Riw"
+province[4] = "Chon buri"
+province[6] = "Chumporn"
+print(province)
