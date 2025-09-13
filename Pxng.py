fb7f8dda2d567351b802614bb0b8840e16d4b7be
Thxtawan <pung242561@gmail.com>
2025-09-13 14:20:35 +0700

    Add files via upload

--- Pxng.py
+++ Pxng.py
@@ -1,19 +1,6 @@
-def add(a, b):
-    return a + b
+def sum_of_squares(numbers):
+    total = 0
+    for n in numbers:
+        total += n ** 2
+    return total
 
-def square(a):
-    return a ** 2
-
-def get_month_name(month_number):
-    months = ["January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December"]
-    if 1 <= month_number <= 12:
-        return months[month_number - 1]
-    else:
-        return "Invalid month number"
-
-def concat_strings(str1, str2, str3):
-    return str1 + str2 + str3
-
-def apply_discount(price, discount):
-    return price - (price * discount / 100)
